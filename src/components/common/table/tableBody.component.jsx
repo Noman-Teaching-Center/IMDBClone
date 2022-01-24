7e22465abbf55574f233c1afc728d2f76aa34414
Noman5237 <anonyman637@gmail.com>
2022-01-24 17:18:10 +0600

    tablebody done

--- src/components/common/table/tableBody.component.jsx
+++ src/components/common/table/tableBody.component.jsx
@@ -1,52 +1,41 @@
 /* eslint-disable react/forbid-prop-types */
 /* eslint-disable no-plusplus */
 import propTypes, { object } from 'prop-types';
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 
-class TableBody extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			tableNonce: TableBody.nonce++,
-		};
-	}
+const TableBody = ({ data, metadata }) => {
+	const [tableNonce] = useState(TableBody.nonce++);
 
-	render() {
-		const { data, metadata } = this.props;
-		let tableKey;
-		try {
-			[[tableKey]] = Object.entries(metadata).filter(
-				([, { key }]) => key === true
-			);
-		} catch (error) {
-			// no handling
-		}
-
-		const renderColumns = Object.entries(metadata)
-			.filter(
-				([col, { render }]) =>
-					render !== undefined &&
-					!(metadata.exclude && metadata.exclude.includes(col))
-			)
-			.map(([key]) => key);
-
-		let nonce = 0;
-		const { tableNonce } = this.state;
-		return (
-			<tbody>
-				{data.map((row) => (
-					<tr key={`tr${tableNonce}${tableKey ? row[tableKey] : nonce}`}>
-						{renderColumns.map((col) => (
-							<td key={`td${nonce++}`}>
-								{metadata[col].render(row[col], row)}
-							</td>
-						))}
-					</tr>
-				))}
-			</tbody>
+	let tableKey;
+	try {
+		[[tableKey]] = Object.entries(metadata).filter(
+			([, { key }]) => key === true
 		);
+	} catch (error) {
+		// no handling
 	}
-}
+
+	const renderColumns = Object.entries(metadata)
+		.filter(
+			([col, { render }]) =>
+				render !== undefined &&
+				!(metadata.exclude && metadata.exclude.includes(col))
+		)
+		.map(([key]) => key);
+
+	let nonce = 0;
+	return (
+		<tbody>
+			{data.map((row) => (
+				<tr key={`tr${tableNonce}${tableKey ? row[tableKey] : nonce}`}>
+					{renderColumns.map((col) => (
+						<td key={`td${nonce++}`}>{metadata[col].render(row[col], row)}</td>
+					))}
+				</tr>
+			))}
+		</tbody>
+	);
+};
 
 TableBody.propTypes = {
 	data: propTypes.arrayOf(object).isRequired,
